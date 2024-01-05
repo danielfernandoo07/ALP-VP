2fb17dadcb587de9c84daf7e4d07cd9e10c73480
Daniel Fernando Herawan <dfernando@student.ciputra.ac.id>
2024-01-05 10:34:03 +0700

    Update CommentSeeder.php

--- laravelAPI/database/seeders/CommentSeeder.php
+++ laravelAPI/database/seeders/CommentSeeder.php
@@ -17,52 +17,52 @@ class CommentSeeder extends Seeder
         Comment::create([
             'content_id' => 1,
             'user_id' => 3,
-            'comment' => 'Open Recruitment NPLC 2023, sangat bagus untuk memperluas partisipasi anggota baru!'
+            'comment_text' => 'Open Recruitment NPLC 2023, sangat bagus untuk memperluas partisipasi anggota baru!'
         ]);
         Comment::create([
             'content_id' => 2,
             'user_id' => 4,
-            'comment' => 'Diharapkan tidak ada lagi info macet, terima kasih.'
+            'comment_text' => 'Diharapkan tidak ada lagi info macet, terima kasih.'
         ]);
         Comment::create([
             'content_id' => 3,
             'user_id' => 1,
-            'comment' => 'Seminar karir di industri teknologi akan sangat bermanfaat!'
+            'comment_text' => 'Seminar karir di industri teknologi akan sangat bermanfaat!'
         ]);
         Comment::create([
             'content_id' => 4,
             'user_id' => 5,
-            'comment' => 'Keren! Workshop kewirausahaan pasti menarik banyak peserta.'
+            'comment_text' => 'Keren! Workshop kewirausahaan pasti menarik banyak peserta.'
         ]);
         Comment::create([
             'content_id' => 5,
             'user_id' => 2,
-            'comment' => 'Kesehatan mental sangat penting, semoga seminar ini sukses!'
+            'comment_text' => 'Kesehatan mental sangat penting, semoga seminar ini sukses!'
         ]);
         Comment::create([
             'content_id' => 6,
             'user_id' => 4,
-            'comment' => 'Studi yang menarik, hubungan antara pola makan dan produktivitas sangat menarik untuk dijelajahi.'
+            'comment_text' => 'Studi yang menarik, hubungan antara pola makan dan produktivitas sangat menarik untuk dijelajahi.'
         ]);
         Comment::create([
             'content_id' => 7,
             'user_id' => 3,
-            'comment' => 'Penting untuk memahami transformasi pendidikan oleh teknologi.'
+            'comment_text' => 'Penting untuk memahami transformasi pendidikan oleh teknologi.'
         ]);
         Comment::create([
             'content_id' => 8,
             'user_id' => 5,
-            'comment' => 'Metode pembelajaran jarak jauh adalah masa depan.'
+            'comment_text' => 'Metode pembelajaran jarak jauh adalah masa depan.'
         ]);
         Comment::create([
             'content_id' => 9,
             'user_id' => 1,
-            'comment' => 'Studi yang sangat relevan dengan kehidupan mahasiswa saat ini.'
+            'comment_text' => 'Studi yang sangat relevan dengan kehidupan mahasiswa saat ini.'
         ]);
         Comment::create([
             'content_id' => 10,
             'user_id' => 2,
-            'comment' => 'Pentingnya fokus pada kesehatan mental di lingkungan akademik.'
+            'comment_text' => 'Pentingnya fokus pada kesehatan mental di lingkungan akademik.'
         ]);
 
     }

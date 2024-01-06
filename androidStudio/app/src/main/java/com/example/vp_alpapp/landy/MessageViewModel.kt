bc29d6a55a6497b4373cd46ffa06af7ad749b55c
yebology <63382728+gentlandy@users.noreply.github.com>
2024-01-07 00:08:38 +0700

    done dm

--- androidStudio/app/src/main/java/com/example/vp_alpapp/landy/MessageViewModel.kt
+++ androidStudio/app/src/main/java/com/example/vp_alpapp/landy/MessageViewModel.kt
@@ -1,7 +1,21 @@
 package com.example.vp_alpapp.landy
 
 import androidx.lifecycle.ViewModel
+import com.example.vp_alpapp.R
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class MessageViewModel : ViewModel() {
+    private val _uistate = MutableStateFlow<List<Message>>(emptyList())
+    val uistate: StateFlow<List<Message>> =_uistate.asStateFlow()
 
+    fun createChat(chat : String) {
+
+        val message = Message(chat)
+        val copy = _uistate.value.toMutableList()
+        copy.add(message)
+        _uistate.value = copy
+
+    }
 }
